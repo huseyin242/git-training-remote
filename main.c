90d4ca9c5cd7165139fcd12cd1c2efbff97efdd6
“Huseyin <huseyinucar242@gmail.com>
2026-08-18 20:37:52 +0200

    Add user information feature

--- main.c
+++ main.c
@@ -8,5 +8,6 @@ int main() {
     printf("Mijn eerste GitHub wijziging!\n");
     printf("Deze wijziging kwam vanaf GitHub!\n");
     printf("Mijn eerste feature branch!\n");
+    printf("User information feature!\n");
     return 0;
 }
